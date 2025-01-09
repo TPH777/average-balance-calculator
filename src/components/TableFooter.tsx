286ee2d5b2f44096b68c3c3805adb5c7a837477e
TPH777 <pinghuitan80@gmail.com>
2025-01-10 02:12:43 +0800

    Change tableFooter css

--- src/components/TableFooter.tsx
+++ src/components/TableFooter.tsx
@@ -1,4 +1,5 @@
 import { getBalanceChange } from "../functions/action";
+import { getDate } from "../functions/date";
 import { roundNumber } from "../functions/number";
 
 interface TableFooterProps {
@@ -18,17 +19,27 @@ export function TableFooter({
   const currEndBalance = roundNumber(lastEndBalance + endChange);
   const currAvgBalance = roundNumber(lastEndBalance + avgChange);
   const avgBalanceDiff = roundNumber(currAvgBalance - lastAvgBalance);
+  const { currMonYr: currMonYr } = getDate(isCurr);
 
   return (
     <tfoot>
       <tr>
-        <td colSpan={3}>End Balance: {currEndBalance}</td>
+        <td style={{ textAlign: "right" }} colSpan={2}>
+          End Balance of {currMonYr}
+        </td>
+        <td>{currEndBalance}</td>
       </tr>
       <tr>
-        <td colSpan={3}>Average Balance: {currAvgBalance}</td>
+        <td style={{ textAlign: "right" }} colSpan={2}>
+          Average Balance of {currMonYr}
+        </td>
+        <td>{currAvgBalance}</td>
       </tr>
       <tr>
-        <td colSpan={3}>Average Balance Change: {avgBalanceDiff}</td>
+        <td style={{ textAlign: "right" }} colSpan={2}>
+          Change in Average Balance of {currMonYr}
+        </td>
+        <td>{avgBalanceDiff}</td>
       </tr>
     </tfoot>
   );
